5eb4d2c0af6d48988e63ee65b5b3ae00e2885461
sneaky-sandwich <126987976+sneaky-sandwich@users.noreply.github.com>
2023-03-04 18:09:04 -0500

    Update app.js

--- src/server/app.js
+++ src/server/app.js
@@ -25,4 +25,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join("../client/build/index.html"));
 });
 
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+})
+
 module.exports = app;
